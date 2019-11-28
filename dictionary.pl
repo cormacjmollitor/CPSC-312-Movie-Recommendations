12a699148e55d897bd974b1c7dc4573cfb70bcfa
Cormac Mollitor <cormacjmollitor@gmail.com>
2019-11-27 16:18:12 -0800

    WIP

--- dictionary.pl
+++ dictionary.pl
@@ -20,7 +20,7 @@ asker('us').
 noun_phrase(P0, P6, Entity, C0, C6) :-
     det(P0, P1, Entity, C0, C1),
     release_date(P1, P2, Entity, C1, C2),
-    director_name(P2, P3, Entity, C2, C3),
+    name(P2, P3, Entity, C2, C3),
     genre(P3, P4, Entity, C3, C4),
     noun(P4, P5, Entity, C4, C5),
     modifying_phrase(P5, P6, Entity, C5, C6).
@@ -29,10 +29,31 @@ det(['a' | P], P, _, C, C).
 det(P, P, _, C, C).
 
 % release_date - check for year (number) or years (number + 's')
-release_date([Num|P], P, _, [number_string(_,Num)|C],C).
+% release_date([Num|P], P, _, [number_string(_, Num)|C], C).
+release_date([Num|P], P, _, [date(Num)|C], C) :- atom_number(Num, _).
+release_date(P, P, _, C, C). % Case for where release date is not present
 
 % check for capitalization to determine if director_name?
-director_name([First, Last|P], P, _, [is_capitalized(First), is_capitalized(Last)|C], C).
+name([First, Middle, Last|P], P, _, [person(FullName)|C], C) :- 
+    string_concat(First, " ", FirstName),
+    string_concat(Middle, " ", MiddleName),
+    string_concat(FirstName, MiddleName, FrontName),
+    string_concat(FrontName, Last, FullName),
+    is_capitalized(First),
+    is_capitalized(Middle),
+    is_capitalized(Last).
+
+name([First, Last|P], P, _, [person(FullName)|C], C) :-
+    string_concat(First, " ", FirstName),
+    string_concat(FirstName, Last, FullName),
+    is_capitalized(First),
+    is_capitalized(Last).
+
+name([First|P], P, _, [person(FirstString)|C], C) :- 
+    is_capitalized(First),
+    atom_string(First, FirstString).
+
+name(P, P, _, C, C). % Case where there isn't a name
 
 % genre is from a hardcoded list of TMDb's recognized genres
 genre([Genre|P], P, _, [get_genre_id(Genre)|C], C).
@@ -44,6 +65,11 @@ noun(['film' | P], P, _, C, C).
 % or 'released in 2006', 'from the 1990s'...
 modifying_phrase(P, P, _, C, C).
 
-% Checks if first letter of a string is capitalized
-is_capitalized(String) :-
-    get_string_code(1,String,First),code_type(First,upper).
+% Checks if first letter of an Atom is capitalized
+is_capitalized(Atom) :-
+    atom_string(Atom, String),
+    get_string_code(1,String,First),
+    code_type(First,upper).
+
+person(P).
+date(D).
