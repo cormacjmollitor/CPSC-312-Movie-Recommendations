a3341694c88c58feaabc2ff31d775f18300c1707
Cormac Mollitor <cormacjmollitor@gmail.com>
2019-11-28 16:28:38 -0800

    Handles different structures of queries now.

--- dictionary.pl
+++ dictionary.pl
@@ -1,4 +1,4 @@
-:- module(dictionary, [starter_phrase/2, noun_phrase/5, modifying_phrase/5]).
+:- module(dictionary, [starter_phrase/2, movie_description/5]).
 :- use_module(get_ids).
 
 starter_phrase(['What', 'is' | P], P).
@@ -11,6 +11,17 @@ starter_phrase([Imperative, Asker | T], T) :-
 starter_phrase(['I', 'want' | P], P).
 starter_phrase(['I', 'want', 'to', 'watch' | P], P).
 
+temporal_phrase(['from', 'before' | P], P).
+temporal_phrase(['made', 'before' | P], P).
+temporal_phrase(['released', 'before' | P], P).
+temporal_phrase(['before' | P], P).
+
+temporal_phrase(['after' | P], P).
+temporal_phrase(['released', 'after' | P], P).
+
+temporal_phrase(['released', 'in' | P], P).
+temporal_phrase(['from' | P], P).
+
 imperative('Give').
 imperative('Suggest').
 imperative('Recommend').
@@ -19,7 +30,7 @@ asker('me').
 asker('us').
 
 % e.g. a 2010 Christopher Nolan crime movie starring Ellen Page
-noun_phrase(P0, P6, Entity, C0, C6) :-
+movie_description(P0, P6, Entity, C0, C6) :-
     det(P0, P1, Entity, C0, C1),
     release_date(P1, P2, Entity, C1, C2),
     celebrity(P2, P3, Entity, C2, C3),
@@ -27,40 +38,22 @@ noun_phrase(P0, P6, Entity, C0, C6) :-
     noun(P4, P5, Entity, C4, C5),
     modifying_phrase(P5, P6, Entity, C5, C6).
 
-% e.g. a crime movie from 2010 starring Ellen Page and directed by Christopher Nolan
-% ['a', 'crime', 'movie', 'from', '2010', 'starring', 'Ellen', 'Page', 'and', 'directed', 'by', 'Christopher', 'Nolan']
-% noun_phrase(P0, P6, Entity, C0, C6) :-
-%     det(P0, P1, Entity, C0, C1),
-%     movie_genre(P3, P4, Entity, C3, C4),
-%     noun(P4, P5, Entity, C4, C5),
-%     release_date(P1, P2, Entity, C1, C2),
-%     celebrity(P2, P3, Entity, C2, C3),
-%     modifying_phrase(P5, P6, Entity, C5, C6).
-
-% e.g. a 2010 movie by Christopher Nolan starring Ellen Page
-% noun_phrase(P0, P6, Entity, C0, C6) :-
-%     det(P0, P1, Entity, C0, C1),
-%     release_date(P1, P2, Entity, C1, C2),
-%     celebrity(P2, P3, Entity, C2, C3),
-%     movie_genre(P3, P4, Entity, C3, C4),
-%     noun(P4, P5, Entity, C4, C5),
-%     modifying_phrase(P5, P6, Entity, C5, C6).
-
 det(['a' | P], P, _, C, C).
 det(P, P, _, C, C).
 
-% Query is looking for release date before specified year
-release_date(['from', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- atom_number(Num, _).
-release_date(['made', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- atom_number(Num, _).
-release_date(['released', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- atom_number(Num, _).
-release_date(['before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- atom_number(Num, _).
+% Query is looking for release date IN specified year
+release_date([Num|P], P, _, [date(Num, 'Year')|C], C) :- number(Num).
+release_date(['from', Num|P], P, _, [date(Num, 'Year')|C], C) :- number(Num).
+release_date(['in', Num|P], P, _, [date(Num, 'Year')|C], C) :- number(Num).
 
 % Query is looking for release date after specified year
-release_date(['after', Num|P], P, _, [date(Num, 'AfterYear')|C], C) :- atom_number(Num, _).
+release_date(['after', Num|P], P, _, [date(Num, 'AfterYear')|C], C) :- number(Num).
 
-% Query is looking for release date IN specified year
-release_date(['from', Num|P], P, _, [date(Num, 'Year')|C], C) :- atom_number(Num, _).
-release_date([Num|P], P, _, [date(Num, 'Year')|C], C) :- atom_number(Num, _).
+% Query is looking for release date before specified year
+release_date(['before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- number(Num).
+release_date(['from', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- number(Num).
+release_date(['made', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- number(Num).
+release_date(['released', 'before', Num|P], P, _, [date(Num, 'BeforeYear')|C], C) :- number(Num).
 
 % Query does not specify a year
 release_date(P, P, _, C, C).
@@ -92,18 +85,28 @@ noun(['film' | P], P, _, C, C).
 
 % modifying_phrase can be 'with ___', 'starring ___', 'by ___', 'directed by ___'...
 % or 'released in 2006', 'from the 1990s'...
-% modifying_phrase(['starring'|P], P, _, celebrity(P, Rest, _, C0, C1), [C0|C], C).
-% modifying_phrase(['from' |P0], P6, _, C0, C6) :-
-%     noun_phrase(P0, P6, _, C0, C6).
-% modifying_phrase(['starring' |P0], P6, _, C0, C6) :-
-%     noun_phrase(P0, P6, _, C0, C6).
-% modifying_phrase(['and' |P0], P6, _, C0, C6) :-
-%     noun_phrase(P0, P6, _, C0, C6).
-modifying_phrase(P, P, _, C, C).
+% Handles punctuation at end of sentence.
+modifying_phrase([], T, _, C, C).
+modifying_phrase([?], T, _, C, C).
+modifying_phrase([.], T, _, C, C).
+modifying_phrase([!], T, _, C, C).
+% Modifying phrases indicating an actor or director
+modifying_phrase(['with'|P], P, _, C0, C2) :- 
+    celebrity(P, T, _, C0, C1),
+    modifying_phrase(T, End, _, C1, C2).
+modifying_phrase(['starring'|P], P, _, C0, C2) :- 
+    celebrity(P, T, _, C0, C1),
+    modifying_phrase(T, End, _, C1, C2).
+modifying_phrase(['directed', 'by'|P], P, _, C0, C2) :- 
+    celebrity(P, T, _, C0, C1),
+    modifying_phrase(T, End, _, C1, C2).
 
-% reln(['starring' | L],L,O1,O2,[borders(O1,O2)|C],C).
-% reln(['directed', 'by' | L],L,O1,O2, [capital(O2,O1)|C],C).
-% reln(['with' | L],L,O1,O2, [borders(O1,O2)|C],C).
+% Modifying phrases about release dates
+modifying_phrase(P, T, _, C0, C2) :- 
+    release_date(P, T, _, C0, C1),
+    modifying_phrase(T, End, _, C1, C2).
+
+modifying_phrase(P, P, _, C, C).
 
 % Checks if first letter of an Atom is capitalized
 is_capitalized(Atom) :-
