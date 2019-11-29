950bbdd766c8eaf3703bceb75cc670d3f3da0fcb
Cormac Mollitor <cormacjmollitor@gmail.com>
2019-11-28 21:08:55 -0800

    Got rid of some singletons

--- dictionary.pl
+++ dictionary.pl
@@ -77,7 +77,7 @@ celebrity(P, P, _, C, C). % Case where there isn't a name
 
 % genre is from a hardcoded list of TMDb's recognized genres
 movie_genre([Genre|P], P, _, [genre(Genre)|C], C) :- 
-    get_genre_id(Genre, Id).
+    get_genre_id(Genre, _).
 movie_genre(Tail, Tail, _, C, C).
 
 noun(['movie' | P], P, _, C, C).
@@ -86,25 +86,25 @@ noun(['film' | P], P, _, C, C).
 % modifying_phrase can be 'with ___', 'starring ___', 'by ___', 'directed by ___'...
 % or 'released in 2006', 'from the 1990s'...
 % Handles punctuation at end of sentence.
-modifying_phrase([], T, _, C, C).
-modifying_phrase([?], T, _, C, C).
-modifying_phrase([.], T, _, C, C).
-modifying_phrase([!], T, _, C, C).
+modifying_phrase([], _, _, C, C).
+modifying_phrase([?], _, _, C, C).
+modifying_phrase([.], _, _, C, C).
+modifying_phrase([!], _, _, C, C).
 % Modifying phrases indicating an actor or director
 modifying_phrase(['with'|P], P, _, C0, C2) :- 
     celebrity(P, T, _, C0, C1),
-    modifying_phrase(T, End, _, C1, C2).
+    modifying_phrase(T, _, _, C1, C2).
 modifying_phrase(['starring'|P], P, _, C0, C2) :- 
     celebrity(P, T, _, C0, C1),
-    modifying_phrase(T, End, _, C1, C2).
+    modifying_phrase(T, _, _, C1, C2).
 modifying_phrase(['directed', 'by'|P], P, _, C0, C2) :- 
     celebrity(P, T, _, C0, C1),
-    modifying_phrase(T, End, _, C1, C2).
+    modifying_phrase(T, _, _, C1, C2).
 
 % Modifying phrases about release dates
 modifying_phrase(P, T, _, C0, C2) :- 
     release_date(P, T, _, C0, C1),
-    modifying_phrase(T, End, _, C1, C2).
+    modifying_phrase(T, _, _, C1, C2).
 
 modifying_phrase(P, P, _, C, C).
 
@@ -113,7 +113,3 @@ is_capitalized(Atom) :-
     atom_string(Atom, String),
     get_string_code(1,String,First),
     code_type(First,upper).
-
-date(Year, BeforeOrAfter).
-person(Name).
-genre(Genre).
--- language.pl
+++ language.pl
@@ -1,15 +1,15 @@
-:- module(language, [promptUser/1]).
+:- module(language, [promptUser/0]).
 :- use_module(api).
 :- use_module(dictionary).
 
 % WIP: Still not hooked up to the API.
-promptUser(Ans) :-
+promptUser() :-
     write("Query for movies: "),
     flush_output(current_output),
     readln(Ln),
-    question(Ln,End, C0, C1),
-    write(C0). % To show that it's parsing correctly.
+    question(Ln, Constraints),
+    write(Constraints). % To show that it's parsing correctly.
 
-question(P0, P3, C0, C1) :-
+question(P0, Constraints) :-
     starter_phrase(P0, P1),
-    movie_description(P1, P2, Params, C0, C1).
+    movie_description(P1, _, _, Constraints, _).
